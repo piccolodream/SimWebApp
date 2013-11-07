528327c2688cbfa381b8f822f80a829867e7cf42
Ulrich Kemloh <piccolo@sweety.local>
2013-11-07 22:00:24 +0100

    Typos corrected
    refactoring.

--- src/org/jupedsim/web/client/SimWebApp.java
+++ src/org/jupedsim/web/client/SimWebApp.java
@@ -20,7 +20,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class SimWebAppa implements EntryPoint {
+public class SimWebApp implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
